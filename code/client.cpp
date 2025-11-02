28f41796b2c8974e6aed746a5ec798a49f6773fd
luluwalulu <553694212@qq.com>
2025-11-02 21:19:01 +0800

    完成：一个简陋的回声聊天室程序

--- code/client.cpp
+++ code/client.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <unistd.h>
 #include <string>
+#include <memory>
 
 using boost::asio::ip::tcp;
 // cin和cout(所有标准iostream对象)共享一把锁
@@ -11,42 +12,55 @@ using boost::asio::ip::tcp;
 
 
 // Receiver的作用就是异步地读取数据并将其打印到控制台
-class Receiver{
+class Receiver
+  :public std::enable_shared_from_this<Receiver>
+{
 public:
   Receiver(boost::asio::io_context& io_context,tcp::socket& socket):
   io_context_(io_context),socket_(socket) {
-    boost::asio::async_read(socket_,boost::asio::buffer(buffer_),[&](const boost::system::error_code& error,std::size_t bytes_transferred){
-      if(!error) receiver_handler(bytes_transferred);
-      else std::cerr<<"读取失败"<<std::endl;
-    });
+    std::cerr<<"Receiver被创立"<<std::endl;
   }
 
-private:
-  void receiver_handler(std::size_t bytes_transferred){
-    boost::asio::async_read(socket_,boost::asio::buffer(buffer_),[&](const boost::system::error_code& error,std::size_t bytes_transferred){
-      if(!error) receiver_handler(bytes_transferred);
+  void start(){
+    std::cerr<<"receive函数开始执行"<<std::endl;
+    boost::asio::async_read_until(socket_,buffer_,'\n',[&,self=shared_from_this()](const boost::system::error_code& error,std::size_t bytes_transferred){
+      // 回调函数体
+      std::istream is(&buffer_);
+      std::string line;
+      std::getline(is,line);
+      if(!error){
+        std::cout<<"收到消息"<<line<<std::endl;
+        self->start();
+      }
       else std::cerr<<"读取失败"<<std::endl;
-    });
-  }
+    }); // async_read语句结束
+  } // start函数结束
 
 private:
   boost::asio::io_context& io_context_;
   tcp::socket& socket_;
-  std::array<char,128> buffer_{};
+  boost::asio::streambuf buffer_;
 };
 
 // Sender的作用是每隔一段固定的时间异步地发送"Hello"（其阻塞点为时间）
-class Sender{
+class Sender
+:public std::enable_shared_from_this<Sender>
+{
 public:
   Sender(boost::asio::io_context& io,tcp::socket& socket,std::string client_name)
     : timer_(io,boost::asio::chrono::seconds(1)),socket_(socket),
       count_(0),buffer_{},client_name_(client_name)
   {
-    if(client_name_.size()!=1) std::cerr<<"名字长度必须为1！"<<std::endl;
-    timer_.async_wait( [this](const boost::system::error_code& /*e*/) {this->send();} );
   }
 
+  void start(){
+    if(client_name_.size()!=1){
+      std::cerr<<"名字长度必须为1！"<<std::endl;
+    } 
+    timer_.async_wait( [self=shared_from_this()](const boost::system::error_code& /*e*/) {self->send();} );
+  }
 private:
+
   void send(){
     if(count_<5){
       // 隐含的问题，一旦client1到服务器的连接建立就发送信息，此时client2和服务器的连接可能尚未建立，所以引入下面的初始身份验证。同时，为了server的转发表能够顺利建立客户名和socket之间的映射
@@ -54,25 +68,26 @@ private:
       // 之后的消息发送"SENDTOTOM  MESSAGE"
       // 前六个字母是行为，后五个字母是客户名，再后面是消息
 
+      // std::cerr<<"开始执行send，此时count_="<<count_<<std::endl;
       if(count_==0){
-        buffer_.assign("L"+client_name_);
+        buffer_.assign("L"+client_name_+'\n');
       }
       else{
-        buffer_.assign("S"+client_name_+"你好你好，我系"+client_name_);
+        buffer_.assign("S"+client_name_+"你好你好，我系"+client_name_+'\n');
       }
-      
-      boost::asio::async_write(socket_,boost::asio::buffer(buffer_),[this](const boost::system::error_code& error,std::size_t bytes_transferred){
-          if(!error) sender_handler();
+      boost::asio::async_write(socket_,boost::asio::buffer(buffer_),[self=shared_from_this()](const boost::system::error_code& error,std::size_t bytes_transferred){
+          if(!error) self->write_handler();
           else std::cerr<<"读取错误"<<std::endl;
         });
       ++count_;      
       timer_.expires_at(timer_.expiry()+boost::asio::chrono::seconds(1));
-      timer_.async_wait([this](const boost::system::error_code& /*e*/) {this->send();} );
+      // 这样套娃shared_from_this是不是有点不对劲啊
+      timer_.async_wait([self=shared_from_this()](const boost::system::error_code& /*e*/) {self->send();} );
     }
   }
 
-  void sender_handler(){
-
+  void write_handler(){
+    // std::cerr<<"结束send函数，此时count_="<<count_-1<<std::endl;
   }
 
 private:
@@ -105,8 +120,12 @@ private:
     // 一旦连接建立，就应该开启async_write和async_read
     // 其中async_write应该被std::cin阻塞，一旦从键盘上读取到数据，就启动async_write发送消息
     // 而async_read不应该被std::cin阻塞
-    Sender(io_context_,socket_,client_name_);
-    Receiver(io_context_,socket_);
+    std::cout<<"连接建立成功"<<std::endl;
+    
+    std::shared_ptr<Receiver> receiver(new Receiver(io_context_,socket_));
+    receiver->start();
+    std::shared_ptr<Sender> sender(new Sender(io_context_,socket_,client_name_));
+    sender->start();
   }
 
 private:
@@ -124,9 +143,20 @@ int main(int argc,char* argv[]){
     }
     boost::asio::io_context io_context;
     Client client(io_context,argv[1]);
+    io_context.run();
   }
   catch(std::exception &e){
     std::cerr<<e.what()<<std::endl;
   }
   return 0;
 }
+
+// 异步地执行任务要避免的一点是，由于类不会阻塞在回调函数那里，所以类可能会被直接销毁。就比如说这里，在Client的handler函数中，
+// Sender类和Receiver类如果不用shared_ptr和shared_from_this处理好的话就会出问题
+
+// 总结一下这里的sender和receiver：
+  // 1.用shared_ptr创建。
+  // 2.继承自enabled_shared_from_this类
+  // 3.在构造函数中禁止使用shared_from_this()函数，构造函数结束后，基类中的弱指针才指向了正确的位置
+  // 4.所以不能依靠类自身的构造函数来完成启动，在类外通过client类来完成启动start()函数
+  // 5.start函数的回调函数需要再次调用start函数
--- code/server.cpp
+++ code/server.cpp
@@ -29,37 +29,74 @@ public:
   // async_accept的回调函数需要调用该函数，然后该连接就会自动地接受和发送消息到对应目标
   void start(){
     // 收到信息：检查目的地，转发信息
-    boost::asio::async_read(socket_,boost::asio::buffer(buffer_),[self=shared_from_this()](const boost::system::error_code& error,
+    std::cerr<<"开始等待消息"<<std::endl;
+    boost::asio::async_read_until(socket_,buffer_,'\n',[self=shared_from_this()](const boost::system::error_code& error,
     std::size_t bytes_transferred){
-      if(!error) self->handler(bytes_transferred);
-      else std::cerr<<"server异步读取消息失败"<<std::endl;
+      if(!error){
+        std::cerr<<"tcp_connection开始工作"<<std::endl;
+        self->handler(bytes_transferred);
+      } 
+      else
+    {
+        // 【“失败”】
+        // 检查是哪种“失败”
+
+        if (error == boost::asio::error::eof) // 是这种！！！！！！！！
+        {
+            // “正常”失败：客户端主动挂断了
+            std::cout << "客户端已正常断开连接。" << std::endl;
+        }
+        else if (error == boost::asio::error::operation_aborted)
+        {
+            // “正常”失败：我们自己关闭了服务器
+            std::cout << "操作被我们自己取消 (服务器关闭中)。" << std::endl;
+        }
+        else if (error == boost::asio::error::connection_reset)
+        {
+            // “异常”失败：客户端崩溃了
+            std::cerr << "客户端连接被重置 (崩溃)。" << std::endl;
+        }
+        else
+        {
+            // “异常”失败：其他所有网络错误
+            std::cerr << "读取错误: " << error.message() << std::endl;
+        }
+
+        // 无论哪种“失败”，这个会话 (tcp_connection) 都应该结束了。
+        // 我们 *不* 再提交下一个读任务。
+        // 这个回调函数返回后，(如果用了 shared_ptr)
+        // 这个会话对象就会被自动销毁。
+    }
     });
   }
 
 private:
   void handler(std::size_t bytes_transferred){
     // 转发buffer_中的消息，然后再次递归地调用start函数
-    if(buffer_[0]=='L'){
-      forward_table_[buffer_[1]]=shared_from_this();
+    std::istream is(&buffer_);
+    std::string line;
+    std::getline(is,line);
+    if(line[0]=='L'){
+      std::cout << "收到消息：L加客户名" << std::endl;
+      forward_table_[line[1]]=shared_from_this();
     }
     else{
-      if(forward_table_.find(buffer_[1])!=forward_table_.end()){
-        std::string temp=std::string(buffer_.begin()+2,buffer_.begin()+bytes_transferred);
-        boost::asio::async_write(forward_table_.find(buffer_[1])->second->socket(),boost::asio::buffer(temp),[](const boost::system::error_code& error,std::size_t bytes_transferred){
-
+      if(forward_table_.find(line[1])!=forward_table_.end()){
+        boost::asio::async_write(forward_table_.find(line[1])->second->socket(),boost::asio::buffer(line+'\n'),[this](const boost::system::error_code& error,std::size_t bytes_transferred){
+          std::cerr<<"消息已经被成功发送"<<std::endl;
         });
       }
       else{
-        std::cerr<<"server尚未与用户"<<buffer_[1]<<"建立连接"<<std::endl;
+        std::cerr<<"server尚未与用户"<<line[1]<<"建立连接"<<std::endl;
       }
     }
-    start();
+    this->start();
   }
 
 private:
   boost::asio::io_context& io_context_;
   tcp::socket socket_;
-  std::array<char,128> buffer_;
+  boost::asio::streambuf buffer_{};
   std::unordered_map<char,pointer>& forward_table_;
 };
 
@@ -77,6 +114,7 @@ private:
     acceptor_.async_accept(new_connection->socket(),
     [=](const boost::system::error_code& error){
       if(!error){
+        std::cout << "server启动tcp_connection" << std::endl;
         new_connection->start();
         // 让对应的socket启动并开始工作，然后继续接受之后的连接请求
         start_accept();
@@ -95,6 +133,7 @@ private:
 int main(){
   boost::asio::io_context io_context;
   tcp_server server(io_context);
+  std::cout << "run之前" << std::endl;
   io_context.run();
   return 0;
 }
